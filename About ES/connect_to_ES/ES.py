d57babd65e482805c9d5cad5141a724dcff654f9
Saayan8981 <saayan8981@gmail.com>
2022-12-05 17:08:19 +0530

    updated

--- About ES/connect_to_ES/ES.py
+++ About ES/connect_to_ES/ES.py
@@ -11,8 +11,8 @@ def send_data_to_es(data):
 
 # Method to get data from elasticsearch
 def get_data_from_es():
- es=Elasticsearch(['http://localhost:9200/'])
- r = es.search(index="employee", body={"query": {"match": {'Name':'Saayan'}}})
+ es=Elasticsearch(['http://localhost:9200/'], http_auth=('elastic', 'Don9891das@'))
+ r = es.search(index="employee", body={"query": {"match": {'Name':'ana'}}})
  print(r)
  print(type(r))
  print(r["hits"]["hits"][0]["_source"])
@@ -22,6 +22,6 @@ if __name__== "__main__":
  # Define a dictoinary having required data to be stored in ES
  data = {"Name": "Ana", "Age":78, "address": "England"}
  # Call method to store data in ES
- send_data_to_es(data)
+ #send_data_to_es(data)
  # Call method to get data from ES
- #get_data_from_es()
+ get_data_from_es()
